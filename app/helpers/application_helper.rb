646ca13d3feaa00209d4be0e79c7b7550c9c17a6
Ife Odugbesan <ifeodugbesan@yahoo.com>
2022-08-28 00:54:40 +0100

    applied correct statistics on stats page, applied fixes from meeting notes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def hide_nav
-    params[:controller] == 'pages' && params[:action] == 'home'
+    (params[:controller] == 'pages' && params[:action] == 'home') || params[:controller] == "devise/sessions" || params[:controller] == "devise/registrations" || params[:controller] == "devise/passwords"
+  end
+
+  def not_devise
+    params[:controller] == "devise/sessions" || params[:controller] == "devise/registrations" || params[:controller] == "devise/passwords"
   end
 end
