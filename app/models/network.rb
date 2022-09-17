80c960d608620f43ea7929f765ba31c4a190ea6b
Ife Odugbesan <ifeodugbesan@yahoo.com>
2022-09-17 17:29:17 +0100

    added validations

--- app/models/network.rb
+++ app/models/network.rb
@@ -2,6 +2,10 @@ class Network < ApplicationRecord
   has_many :waterpoints, dependent: :destroy
   has_many :tasks
   has_many :services, through: :tasks
+  validates :name, presence: :true
+  validates :address, presence: :true
+  validates :number_of_households, presence: :true
+  validates :max_households, presence: :true
   paginates_per 20
   include PgSearch::Model
   pg_search_scope :search_by_name,
--- app/models/waterpoint.rb
+++ app/models/waterpoint.rb
@@ -3,4 +3,14 @@ class Waterpoint < ApplicationRecord
   has_many :tasks
   has_many :waterpoint_equipments
   has_many :equipments, through: :waterpoint_equipments
+  validates :name, presence: true
+  validates :network, presence: true
+  validates :waterpoint_type, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :address, presence: true
+  validates :manufacturer, presence: true
+  validates :efficiency, presence: true
+  validates :design_period, presence: true
+  validates :warranty, presence: true
 end
