c98e021d80557ea493f219e0fc0035bfaef867ba
Ife Odugbesan <ifeodugbesan@yahoo.com>
2023-07-03 11:15:36 +0100

    added completion date to edit tasks

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,9 +4,23 @@ import flatpickr from "flatpickr";
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   // Inform the controller that it has two targets in the form, which are our inputs
-  static targets = [ "completedDate", "completedDateMobile" ]
+  static targets = [ "startDate", "endDate", "completedDate", "completedDateMobile" ]
 
   connect() {
+    if (this.hasStartDateTarget) {
+      flatpickr(this.startDateTarget, {
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+      })
+    }
+    if (this.hasEndDateTarget) {
+      flatpickr(this.endDateTarget, {
+        altInput: true,
+        altFormat: "F j, Y",
+        dateFormat: "Y-m-d",
+      })
+    }
     if (this.hasCompletedDateTarget) {
       flatpickr(this.completedDateTarget, {
         altInput: true,
